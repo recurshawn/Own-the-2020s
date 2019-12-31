998f1cfbe4e042e0ff782a3685d44078640b0f80
Shawn Pereira <recurshawn@users.noreply.github.com>
2019-12-31 20:32:07 +0530

    Structure of Output and Footer done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import React from 'react';
 import NavBar from './components/NavBar';
+import Footer from './components/Footer';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <NavBar />
-      <br/>
-        <div className="Input wrapper block fixed">
-          <p>I plan on giving up <input className="wrapper block fixed inline" type="number" name="hours" /> hours of <input className="wrapper block fixed inline" type="text" name="habit" /> per <select className="wrapper block fixed inline">
-            <option value="volvo">week</option>
-            <option value="saab">day</option>
-            <option value="opel">month</option>
+class App extends React.Component {
+
+  state = {
+    habit : "Netflix",
+    totalHours: 0,
+  }
+
+  calculate = () => {
+    
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <NavBar />
+        <br />
+        <div className="wrapper block fixed Input">
+          <p>I plan on giving up <input className="wrapper block fixed inline" type="number" name="hours" placeholder="2" /> hours of <input className="wrapper block inline fixed" type="text" name="habit" placeholder="Netflix" /> per <select className="wrapper block fixed inline">
+            <option value="week">week</option>
+            <option value="day">day</option>
+            <option value="month">month</option>
           </select> in the 2020s.</p>
-          <br/>
-          <button className="block accent">Calculate free time</button>
+          <br />
+          <button onClick={this.state.calculate} className="block accent">Calculate free time</button>
         </div>
-      
-    </div>
-  );
+        <br />
+        <div className="Output wrapper block fixed">
+          <p>You can save X hours per month, 12X hours per year AND 120X hours in the 2020s!</p>
+          <p>You can learn to code and get a tech job with that time!</p>
+        </div>
+        <br/>
+        <Footer />
+      </div>
+    );
+  }
+
 }
 
 export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+    return(
+    <div className="Footer">
+        <p>Made with ❤️ by Shawn Pereira <a href="https://twitter.com/Incept_shawn">@Incept_shawn</a>
+        <br/><a target="_blank" rel="noopener noreferrer" href="https://ko-fi.com/recurshawn">🚀 Support this project!</a></p>
+        <a href="https://shawnpereira.me">shawnpereira.me</a>
+        <br/>2020 © Shawn Pereira | MIT License
+    </div>
+    );
+}
+
+export default Footer;
