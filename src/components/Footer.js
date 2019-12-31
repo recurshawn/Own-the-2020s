d7c46bfb98a282384f6d748e88b0903218c15064
Shawn Pereira <recurshawn@users.noreply.github.com>
2019-12-31 21:32:03 +0530

    Product Hunt badge placeholder

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,11 @@ import React from 'react';
 const Footer = () => {
     return(
     <div className="Footer">
+        <div className="ProductHunt wrapper block fixed">
+            <p>Liked this? Upvote it on Product Hunt!</p>
+            <p> INSERT PRODUCT HUNT BADGE HERE</p>
+            <p>Have an awesome 2020 and the decade ahead!</p>
+        </div>
         <p>Made with ❤️ by Shawn Pereira <a href="https://twitter.com/Incept_shawn">@Incept_shawn</a>
         <br/><a target="_blank" rel="noopener noreferrer" href="https://ko-fi.com/recurshawn">🚀 Support this project!</a></p>
         <a href="https://shawnpereira.me">shawnpereira.me</a>
