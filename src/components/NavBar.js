69a7337b8d003fb182a096752a8c1edd53ee54b6
Shawn Pereira <recurshawn@users.noreply.github.com>
2020-01-01 11:00:35 +0530

    Logo and icons

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,7 @@ const NavBar = () => {
 
     return(
         <div className="NavBar">
-            <h1>Own the 2020s</h1>
+            <h1>🚀Own the <br/> &nbsp; &nbsp; 2020s</h1>
         </div>
     );
 
