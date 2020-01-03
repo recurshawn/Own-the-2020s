47886e3196e07fa112dc49180e79003d52a46603
Shawn Pereira <recurshawn@users.noreply.github.com>
2020-01-03 21:46:28 +0530

    Changed Product Hunt badge

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
         <div className="Footer">
             <div className="ProductHunt wrapper block fixed">
                 <p>Liked this? Upvote it on Product Hunt!</p>
-                <a href="https://www.producthunt.com/posts/own-the-2020s?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-own-the-2020s" target="_blank" rel="noopener noreferrer"><img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=179562&theme=light" alt="Own the 2020s! - This decade, give up time to gain time | Product Hunt Embed" width="250px" height="54px" /></a>
+                <a href="https://www.producthunt.com/posts/own-the-2020s?utm_source=badge-top-post-badge&utm_medium=badge&utm_souce=badge-own-the-2020s" target="_blank"><img src="https://api.producthunt.com/widgets/embed-image/v1/top-post-badge.svg?post_id=179562&theme=dark&period=daily" alt="Own the 2020s - This decade, give up time to gain time | Product Hunt Embed" width="250px" height="54px" /></a>
                 <p>Have an awesome 2020 and the decade ahead!</p>
             </div>
             <br />
